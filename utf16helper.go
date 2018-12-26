8bc46cad43a73fc82098740a70af0d3a0d1bbd80
Frank Xu <frank.xu@northbright.com>
2018-12-26 13:21:10 +0800

    Add UTF16ToUTF8Ctx()
    * Rename ReadUTF16BOM() to DetectUTF16BOM()
    * Add UTF8ToUTF16()
    * Add UTF16ToUTF8Ctx()
    * Add UTF16ToUTF8()
    * Add test cases

--- utf16helper.go
+++ utf16helper.go
@@ -8,20 +8,20 @@ import (
 	"fmt"
 	"io"
 	"unicode/utf16"
-	//"unicode/utf8"
+	"unicode/utf8"
 	"unsafe"
 
 	"github.com/northbright/byteorder"
 )
 
 var (
-	UTF16LE  = [2]byte{0xFF, 0xFE}
-	UTF16BE  = [2]byte{0xFE, 0xFF}
-	UTF8BOM  = [3]byte{0xEF, 0xBB, 0xBF}
-	ErrNoBOM = fmt.Errorf("No UTF-16 BOM found")
+	UTF16LE       = [2]byte{0xFF, 0xFE}
+	UTF16BE       = [2]byte{0xFE, 0xFF}
+	UTF8BOM       = [3]byte{0xEF, 0xBB, 0xBF}
+	ErrNoUTF16BOM = fmt.Errorf("No UTF-16 BOM found")
 )
 
-func ReadUTF16BOM(r io.Reader) ([]byte, binary.ByteOrder, error) {
+func DetectUTF16BOM(r io.Reader) (binary.ByteOrder, error) {
 	var buf []byte
 
 	reader := bufio.NewReader(r)
@@ -30,18 +30,18 @@ func ReadUTF16BOM(r io.Reader) ([]byte, binary.ByteOrder, error) {
 	for i := 0; i < 2; i++ {
 		b, err := reader.ReadByte()
 		if err != nil {
-			return nil, nil, err
+			return nil, err
 		}
 		buf = append(buf, b)
 	}
 
 	switch {
 	case buf[0] == 0xFF && buf[1] == 0xFE:
-		return buf, binary.LittleEndian, nil
+		return binary.LittleEndian, nil
 	case buf[0] == 0xFE && buf[1] == 0xFF:
-		return buf, binary.BigEndian, nil
+		return binary.BigEndian, nil
 	default:
-		return buf, nil, nil
+		return nil, ErrNoUTF16BOM
 	}
 }
 
@@ -50,13 +50,13 @@ func WriteUTF16BOM(order binary.ByteOrder, dst io.Writer) error {
 
 	switch order {
 	case nil:
-		return ErrNoBOM
+		return ErrNoUTF16BOM
 	case binary.LittleEndian:
 		BOM = UTF16LE[0:2]
 	case binary.BigEndian:
 		BOM = UTF16BE[0:2]
 	default:
-		return ErrNoBOM
+		return ErrNoUTF16BOM
 	}
 
 	_, err := dst.Write(BOM)
@@ -128,4 +128,66 @@ func UTF8ToUTF16Ctx(ctx context.Context, src io.Reader, dst io.Writer) error {
 	}
 
 	return writer.Flush()
+}
+
+func UTF8ToUTF16(src io.Reader, dst io.Writer) error {
+	return UTF8ToUTF16Ctx(context.Background(), src, dst)
+}
+
+func UTF16ToUTF8Ctx(ctx context.Context, src io.Reader, dst io.Writer, outputUTF8BOM bool) error {
+	var (
+		buf       = make([]byte, 2)
+		utf8Buf   = make([]byte, 4)
+		uint16Buf = make([]uint16, 1)
+	)
+
+	reader := bufio.NewReader(src)
+	writer := bufio.NewWriter(dst)
+
+	// Output UTF-8 BOM if need(E.g. Plain TXT files for Windows)
+	if outputUTF8BOM {
+		if err := WriteUTF8BOM(writer); err != nil {
+			return err
+		}
+	}
+
+	order, err := DetectUTF16BOM(reader)
+	if err != nil {
+		return err
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+		}
+
+		i := 0
+		for i = 0; i < 2; i++ {
+			buf[i], err = reader.ReadByte()
+		}
+
+		if err != nil && err != io.EOF {
+			return err
+		}
+
+		if err == io.EOF {
+			break
+		}
+
+		uint16Buf[0] = order.Uint16(buf[0:])
+		runes := utf16.Decode(uint16Buf)
+
+		for _, r := range runes {
+			n := utf8.EncodeRune(utf8Buf, r)
+			dst.Write(utf8Buf[:n])
+		}
+	}
+
+	return writer.Flush()
+}
+
+func UTF16ToUTF8(src io.Reader, dst io.Writer, outputUTF8BOM bool) error {
+	return UTF16ToUTF8Ctx(context.Background(), src, dst, outputUTF8BOM)
 }
--- utf16helper_test.go
+++ utf16helper_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/northbright/utf16helper"
 )
 
-func ExampleReadUTF16BOM() {
+func ExampleDetectUTF16BOM() {
 	arr := [][]byte{
 		// UTF-16 string: "服" with little endian BOM.
 		[]byte{0xFF, 0xFE, 0x0d, 0x67},
@@ -22,12 +22,12 @@ func ExampleReadUTF16BOM() {
 
 	for _, v := range arr {
 		buf := bytes.NewBuffer(v)
-		bom, order, err := utf16helper.ReadUTF16BOM(buf)
+		order, err := utf16helper.DetectUTF16BOM(buf)
 		if err != nil {
-			log.Printf("ReadUTFBOM() error: %v", err)
+			log.Printf("DtectUTF16BOM() error: %v", err)
 			return
 		}
-		log.Printf("BOM(first 2 bytes): %X, order: %v", bom, order)
+		log.Printf("order: %v", order)
 	}
 
 	// Output:
@@ -45,12 +45,12 @@ func ExampleWriteUTF16BOM() {
 		buf := &bytes.Buffer{}
 
 		err := utf16helper.WriteUTF16BOM(order, buf)
-		if err != nil && err != utf16helper.ErrNoBOM {
+		if err != nil && err != utf16helper.ErrNoUTF16BOM {
 			log.Printf("WriteUTF16BOM() error: %v", err)
 			return
 		}
 
-		if err != utf16helper.ErrNoBOM {
+		if err != utf16helper.ErrNoUTF16BOM {
 			fmt.Printf("written BOM: 0x%X\n", buf.Bytes())
 		} else {
 			fmt.Printf("no BOM\n")
@@ -81,12 +81,16 @@ func ExampleWriteUTF8BOM() {
 func ExampleUTF8ToUTF16Ctx() {
 	// Get an empty context.
 	ctx := context.Background()
+
+	// Case 1: Use UTF8 bytes directly.
 	// "Hello, 世界!" in UTF8 with BOM(written in Notepad.exe on Windows).
 	utf8Bytes := []byte{0xEF, 0xBB, 0xBF, 0x48, 0x65, 0x6c, 0x6c, 0x6f,
 		0x2c, 0x20, 0xe4, 0xb8, 0x96, 0xe7, 0x95, 0x8c, 0x21}
 	buf := []byte{}
 
+	// Create a bytes reader(io.Reader) from UTF8 bytes.
 	reader := bytes.NewReader(utf8Bytes)
+	// Create a buffer writer(io.Writer) to contain converted UTF16 bytes.
 	writer := bytes.NewBuffer(buf)
 
 	if err := utf16helper.UTF8ToUTF16Ctx(ctx, reader, writer); err != nil {
@@ -94,6 +98,47 @@ func ExampleUTF8ToUTF16Ctx() {
 		return
 	}
 
-	log.Printf("%X\n", writer.Bytes())
+	log.Printf("%X", writer.Bytes())
+
+	// Case 2: Convert string to UTF8 bytes.
+	utf8Bytes = []byte(`Hello, 世界!`)
+	buf = []byte{}
+
+	// Create a bytes reader(io.Reader) from UTF8 bytes.
+	reader = bytes.NewReader(utf8Bytes)
+	// Create a buffer writer(io.Writer) to contain converted UTF16 bytes.
+	writer = bytes.NewBuffer(buf)
+
+	if err := utf16helper.UTF8ToUTF16Ctx(ctx, reader, writer); err != nil {
+		log.Printf("UTF8ToUTF16Ctx() error: %v", err)
+		return
+	}
+
+	log.Printf("%X", writer.Bytes())
+
+	// Output:
+}
+
+func ExampleUTF16ToUTF8Ctx() {
+	// Get an empty context.
+	ctx := context.Background()
+	// "Hello, 世界!" in UTF16 with UTF16LE(written in Notepad.exe on Windows).
+	utf16Bytes := []byte{0xFF, 0xFE, 0x48, 0x00, 0x65, 0x00, 0x6c, 0x00, 0x6c, 0x00,
+		0x6f, 0x00, 0x2c, 0x00, 0x20, 0x00, 0x16, 0x4e, 0x4c, 0x75, 0x21, 0x00}
+	buf := []byte{}
+
+	// Create a bytes reader(io.Reader) from UTF16 bytes.
+	reader := bytes.NewReader(utf16Bytes)
+	// Create a buffer writer(io.Writer) to contain converted UTF8 bytes.
+	writer := bytes.NewBuffer(buf)
+
+	if err := utf16helper.UTF16ToUTF8Ctx(ctx, reader, writer, false); err != nil {
+		log.Printf("UTF16ToUTF8Ctx() error: %v", err)
+		return
+	}
+
+	log.Printf("%X", writer.Bytes())
+	// Output string converted from UTF8 bytes.
+	log.Printf("string: %s", writer.String())
 	// Output:
 }
